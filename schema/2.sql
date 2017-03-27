b6461b93a2cd6db285c8772ef7825d486f843b0c
John Clark <socrates37@gmail.com>
2017-03-27 14:58:25 -0600

    put migrations in place

---
+++ schema/2.sql
@@ -0,0 +1,20 @@
+# Codes
+CREATE TABLE IF NOT EXISTS codes (
+  code VARCHAR(64) NOT NULL,
+  user_id INT UNSIGNED NOT NULL,
+  expires DATETIME NOT NULL,
+  PRIMARY KEY (code),
+  KEY (expires)
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+# Tokens
+CREATE TABLE IF NOT EXISTS codes (
+  token VARCHAR(36) NOT NULL,
+  user_id INT UNSIGNED NOT NULL,
+  created DATETIME DEFAULT CURRENT_TIMESTAMP,
+  expires DATETIME NOT NULL,
+  description VARCHAR(128),
+  PRIMARY KEY (token),
+  KEY (user_id),
+  KEY (expires)
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
