a1046dc52cd8d2addb1362eff492b5eb48c56953
John Clark <socrates37@gmail.com>
2017-03-30 13:04:52 -0600

    prep for api tests

--- api/query/user.test.js
+++ api/query/user.test.js
@@ -1,7 +1,18 @@
 
 
 describe('api/user', () => {
-  it('should work', async () => {
+  before(async () => {
+    console.log(`truncating ${process.env.MYSQL_DATABASE}`);
     
-  })
-})
+  });
+
+  it('should error when unauthorized');
+
+  it('should get user by id');
+
+  it('should get user by email');
+
+  it('should get user by username');
+
+  it('should get authorized user');
+});
