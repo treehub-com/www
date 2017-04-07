55fbc36a2b0436997439bbfa807ce7cd522d59f8
John Clark <socrates37@gmail.com>
2017-04-07 10:51:24 -0600

    increase body size limit to 5mb; closes #18

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ router.get('*', async (ctx) => {
 });
 
 app
-  .use(bodyparser({detectJSON: (ctx) => true}))
+  .use(bodyparser({detectJSON: (ctx) => true, jsonLimit: '5mb'}))
   .use(router.routes())
   .use(router.allowedMethods());
 
